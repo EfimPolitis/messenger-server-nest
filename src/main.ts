207e79358a2d76034c1e83b7a488c6a3e184677f
Efim <efempolites@gmail.com>
2025-06-05 09:47:23 +0300

    add Dockerfile.dev

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,29 @@
-import { RequestMethod } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-import * as cookieParser from 'cookie-parser'
-import { AppModule } from './app.module'
+import { RequestMethod } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-	app.setGlobalPrefix('api', {
-		exclude: [
-			{ path: 'auth/google', method: RequestMethod.GET },
-			{ path: 'auth/google/redirect', method: RequestMethod.GET },
-			{ path: 'auth/github', method: RequestMethod.GET },
-			{ path: 'auth/github/redirect', method: RequestMethod.GET },
-			{ path: 'verify-email', method: RequestMethod.GET }
-		]
-	})
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: 'auth/google', method: RequestMethod.GET },
+      { path: 'auth/google/redirect', method: RequestMethod.GET },
+      { path: 'auth/github', method: RequestMethod.GET },
+      { path: 'auth/github/redirect', method: RequestMethod.GET },
+      { path: 'verify-email', method: RequestMethod.GET },
+    ],
+  });
 
-	app.use(cookieParser())
-	app.enableCors({
-		origin: ['http://localhost:3000'],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
+  app.use(cookieParser());
+  app.enableCors({
+    // origin: ['http://localhost:3000'],
+    origin: ['https://quick-online.ru'],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
 
-	await app.listen(4200)
+  await app.listen(4200);
 }
-bootstrap()
+bootstrap();
